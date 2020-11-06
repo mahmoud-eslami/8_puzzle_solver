36d81f0e082982bfe11e5d63e25fad28b1db9a84
mahmoud-eslami <eslamimahmoud98@gmail.com>
2020-11-06 09:08:43 +0330

    delete trash comment

--- src/App.java
+++ src/App.java
@@ -2,12 +2,11 @@ import java.util.ArrayList;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        // int[][] testNodeInfo = { { 7, 6, 4 }, { 5, 2, 1 }, { 0, 3, 8 } }; solve
-        // int[][] testNodeInfo = { { 8, 0, 4 }, { 2, 5, 6 }, { 3, 1, 7 } }; java out of
-        // memory
-        // int[][] testNodeInfo = { { 3, 6, 5 }, { 1, 7, 4 }, { 0, 2, 8 } }; solve
-        // int[][] testNodeInfo = { { 2, 0, 1 }, { 4, 3, 7 }, { 6, 5, 8 } }; not
-        // solvable
+        // example
+        // int[][] testNodeInfo = { { 7, 6, 4 }, { 5, 2, 1 }, { 0, 3, 8 } }; 
+        // int[][] testNodeInfo = { { 8, 0, 4 }, { 2, 5, 6 }, { 3, 1, 7 } }; 
+        // int[][] testNodeInfo = { { 3, 6, 5 }, { 1, 7, 4 }, { 0, 2, 8 } }; 
+        // int[][] testNodeInfo = { { 2, 0, 1 }, { 4, 3, 7 }, { 6, 5, 8 } }; 
 
         int[][] testNodeInfo = { { 3, 6, 5 }, { 1, 7, 4 }, { 0, 2, 8 } };
         int[][] goalNodeInfo = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 0 } };
